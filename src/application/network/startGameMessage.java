db5320e2fc58baf14d98f3752900583677af6afd
Swini <Aswina.Zizzari@adesso.ch>
2017-01-13 08:45:43 +0100

    rename

--- src/application/network/startGameMessage.java
+++ src/application/network/startGameMessage.java
@@ -1,10 +1,39 @@
 package application.network;
 
+import java.util.Random;
+
 import network.Message;
 
-public class startGameMessage implements Message {
+public class StartGameMessage implements Message {
 
-	public startGameMessage() {
+	private String[][] labyrinthData;
+	private Random random = new Random();
+
+	public StartGameMessage() {
 		super();
+		labyrinthData = new String[13][13];
+		
+		for (int i = 0; i < 13 ; i++){			
+			for (int j = 0; j < 13 ; j++){
+				if (i%2 != 0 && j%2 != 0){
+					labyrinthData[i][j] = "SB";
+				}else{
+					labyrinthData[i][j] = fillInTestData();
+				}
+			}
+		}
+			labyrinthData[0][0] = "PL";
+			labyrinthData[0][12] = "PL";
+			labyrinthData[12][0] = "PL";
+			labyrinthData[12][12] = "PL";
+	}
+
+	public String[][] getLabyrinthData() {
+		return labyrinthData;
+	}
+	
+	private String fillInTestData(){
+		String[] testData = {"NB","",};
+		return testData[random.nextInt(testData.length)];
 	}
 }
